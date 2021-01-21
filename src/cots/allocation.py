83d37cf69b8ebcc946a2727dbc3844899143e156
Etienne Leclercq <etienne.leclercq@alterway.fr>
2021-01-21 18:09:43 +0100

    generic build df_host + smaller data

---
+++ src/cots/allocation.py
@@ -0,0 +1,16 @@
+"""
+=========
+cots allocation
+=========
+
+Provide resource allocation related functions to handle this problem.
+"""
+
+
+def constraints_satisfied(config) -> bool:
+    """Check if allocation constraints are satisfied or not."""
+    satisfied = True
+    print(config)
+    print(type(config))
+
+    return satisfied
--- src/cots/main.py
+++ src/cots/main.py
@@ -23,6 +23,7 @@ from matplotlib import pyplot as plt
 import pandas as pd
 
 # Personnal imports
+from . import allocation as alloc
 from . import clustering as clt
 from . import container as ctnr
 from . import init as it
@@ -48,7 +49,7 @@ def main(data, params):
     main_time = time.time()
 
     config = it.read_params(params)
-    it.define_globals(config)
+    # it.define_globals(config)
     plt.style.use('bmh')
     # pd.options.plotting.backend = 'plotly'  # usefull ?
 
@@ -147,6 +148,10 @@ def main(data, params):
                                my_instance.df_indiv[it.tick_field].max())
 
     # input('\nEnd of first part, press enter to enter loop ...\n')
+
+    # Test allocation use case
+    print(alloc.constraints_satisfied(config['allocation']))
+    input()
 
     # loop 'streaming' progress
     streaming_eval(my_instance, df_indiv_clust, labels_,

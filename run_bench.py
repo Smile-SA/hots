33d5c7a8d1a6332f8ff4e5e088ddf85d132a0762
Etienne Leclercq <etienne.leclercq@alterway.fr>
2021-10-17 00:46:12 +0200

    several alternatives for loop
    Former-commit-id: f9eba5fee6f254ef4d413dfa0d53fbf1ec828a72

--- run_bench.py
+++ run_bench.py
@@ -27,7 +27,12 @@ def main(path, kmin, kmax, taumin, taumax, kstep, taustep):
 
     for k in range(kmin, kmax + 1, kstep):
         for tau in range(taumin, taumax + 1, taustep):
-            bash_command = 'cots --path ' + path + ' --k ' + str(k) + ' --tau ' + str(tau)
+            bash_command = 'cots ' + path + ' -k ' + str(k) + ' --tau ' + str(tau)
+            print('\n%s\n' % bash_command)
+            process = subprocess.Popen(bash_command.split())
+            output, error = process.communicate()
+
+            bash_command = 'cots ' + path + ' -k ' + str(k) + ' -t ' + str(tau) + ' -m loop_kmeans'
             print('\n%s\n' % bash_command)
             process = subprocess.Popen(bash_command.split())
             output, error = process.communicate()

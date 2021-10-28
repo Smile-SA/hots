fcb99717fa1173de841422ed292eea91591a6323
Etienne Leclercq <etienne.leclercq@alterway.fr>
2021-10-28 16:58:37 +0200

    clean some tests files + docstring in conftest
    Former-commit-id: a33fcf273c01c95f318225ccab7133071e2ca0eb

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,14 +11,17 @@ import pytest
 
 @pytest.fixture
 def tests_directory():
+    """Get path of parent dir."""
     return pathlib.Path(__file__).resolve().parent
 
 
 @pytest.fixture
 def package_directory(tests_directory):
+    """Get test paretn dir"""
     return tests_directory.parent
 
 
 @pytest.fixture
 def tests_data_directory(tests_directory):
-    return tests_directory / "data"
+    """Check if data dir is in test dir."""
+    return tests_directory / 'data'

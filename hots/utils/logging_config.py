d5376d25483ff0db226331ad1a3660fcfe3e4aa1
Etienne Leclercq <etienne.leclercq@smile.fr>
2025-09-02 11:52:51 +0200

    fix logging creation

--- hots/utils/logging_config.py
+++ hots/utils/logging_config.py
@@ -1,10 +1,12 @@
 """HOTS logging configuration."""
 
 import logging
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
 from typing import Optional
 
 
-def setup_logging(level: str, filename: Optional[str], fmt: str) -> None:
+def setup_logging(level: str, filename: Optional[str], fmt: str, add_console: bool = False) -> None:
     """
     Initialize Python root logger.
     :param level: e.g. "INFO" or "DEBUG"
@@ -13,13 +15,37 @@ def setup_logging(level: str, filename: Optional[str], fmt: str) -> None:
     """
     numeric_level = getattr(logging, level.upper(), logging.INFO)
     handlers = []
+    formatter = logging.Formatter(fmt)
+
     if filename:
-        handlers.append(logging.FileHandler(filename))
+        p = Path(filename)
+        if p.parent and not p.parent.exists():
+            p.parent.mkdir(parents=True, exist_ok=True)
+        
+        file_handler = RotatingFileHandler(
+            p,
+            maxBytes=5_000_000,
+            backupCount=3,
+            encoding='utf-8'
+        )
+        file_handler.setLevel(numeric_level)
+        file_handler.setFormatter(formatter)
+        handlers.append(file_handler)
+
+        if add_console:
+            sh = logging.StreamHandler()
+            sh.setLevel(numeric_level)
+            sh.setFormatter(formatter)
+            handlers.append(sh)
+        
     else:
-        handlers.append(logging.StreamHandler())
+        sh = logging.StreamHandler()
+        sh.setLevel(numeric_level)
+        sh.setFormatter(formatter)
+        handlers.append(sh)
 
     logging.basicConfig(
         level=numeric_level,
-        format=fmt,
         handlers=handlers,
+        force=True
     )

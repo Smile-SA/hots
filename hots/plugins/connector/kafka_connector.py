caec4a8181b0984efe340245037006dfc2995910
Etienne Leclercq <etienne.leclercq@smile.fr>
2025-11-06 16:21:17 +0100

    merge kafka connector and reader

--- hots/plugins/connector/kafka_connector.py
+++ hots/plugins/connector/kafka_connector.py
@@ -4,21 +4,60 @@
 
 import json
 
-from confluent_kafka import Producer
+from confluent_kafka import Consumer, KafkaError, KafkaException, Producer
 
 from hots.core.interfaces import ConnectorPlugin
 
+import pandas as pd
+
 
 class KafkaConnector(ConnectorPlugin):
-    """Connector plugin that publishes moves to Kafka."""
+    """Connector plugin that read data from Kafka and publishes moves to it."""
 
-    def __init__(self, params, instance):
+    def __init__(self, params):
         """Initialize Kafka producer and topic."""
+        self.consumer = Consumer(params['consumer_conf'])
+        self.consumer.subscribe(params['topics'])
+        self.tick_field = params['tick_field']
+        self.indiv_field = params['individual_field']
+        self.host_field = params['host_field']
+        self.metrics = params['metrics']
+        self.batch_size = params.get('batch_size', 1)
+
         self.producer = Producer(
             {'bootstrap.servers': params['bootstrap.servers']}
         )
         self.topic = params['topics'][0]
         self.pending_changes = {}
+
+    def load_initial(self) -> pd.DataFrame:
+        """Load initial data batch (no‑op, returns empty)."""
+        return pd.DataFrame(), None, None
+
+    def load_next(self) -> pd.DataFrame:
+        """Poll Kafka and return the next DataFrame of container records."""
+        records = []
+        for _ in range(self.batch_size):
+            msg = self.consumer.poll(1.0)
+            if msg is None:
+                continue
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    continue
+                raise KafkaException(msg.error())
+
+            rec = json.loads(msg.value())
+            for c in rec.get('containers', []):
+                records.append({
+                    self.tick_field: c['timestamp'],
+                    self.indiv_field: c['container_id'],
+                    self.host_field: c['machine_id'],
+                    self.metrics[0]: c[self.metrics[0]],
+                })
+
+        if not records:
+            return None
+        return pd.DataFrame.from_records(records)
 
     def apply_moves(self, moves):
         """Publish moves (list of dicts) to Kafka."""
